3c586288f8bc7f616d3a62b0446ef254e3bf1e1b
M-Laetitia <molin.laetitia@gmail.com>
2024-05-07 21:24:36 +0200

    Feat: add win location

--- Role-playing-game/script.js
+++ Role-playing-game/script.js
@@ -79,6 +79,12 @@ const locations = [
     "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
     "button functions": [restart, restart, restart],
     text: "You die. &#x2620;"
+  },
+  { 
+    name: "win", 
+    "button text": ["REPLAY?", "REPLAY?", "REPLAY?"], 
+    "button functions": [restart, restart, restart], 
+    text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;" 
   }
 ];
 
@@ -95,7 +101,7 @@ function update(location) {
     button1.onclick = location["button functions"][0];
     button2.onclick = location["button functions"][1];
     button3.onclick = location["button functions"][2];
-    text.innerText = location.text;
+    text.innerHTML = location.text;
 }
   
 function goTown() {
