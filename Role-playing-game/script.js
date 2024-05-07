4727d92793e7958b04537f7c12043233f689e4bc
M-Laetitia <molin.laetitia@gmail.com>
2024-05-07 10:57:55 +0200

    Feat: buyWeapon function

--- Role-playing-game/script.js
+++ Role-playing-game/script.js
@@ -60,27 +60,47 @@ function update(location) {
     button2.onclick = location["button functions"][1];
     button3.onclick = location["button functions"][2];
     text.innerText = location.text;
-  }
+}
   
-  function goTown() {
-    update(locations[0]);
-  }
+function goTown() {
+  update(locations[0]);
+}
   
-  function goStore() {
-    update(locations[1]);
-  }
+function goStore() {
+  update(locations[1]);
+}
   
-  function goCave() {
-    update(locations[2]);
-  }
+function goCave() {
+  update(locations[2]);
+}
 
-  function buyHealth() {
-    if (gold >= 10) {
-      gold -= 10;
-      health += 10;
-      goldText.innerText = gold;
-      healthText.innerText = health;
-    } else {
-      text.innerText = "You do not have enough gold to buy health.";
-    }
+function buyHealth() {
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  } else {
+    text.innerText = "You do not have enough gold to buy health.";
   }
+}
+
+function buyWeapon() {
+  if (currentWeapon < weapons.length - 1) {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeapon++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeapon].name;
+      text.innerText = "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon.";
+    }
+  } else {
+    text.innerText = "You already have the most powerful weapon!";
+    button2.innerText = "Sell weapon for 15 gold";
+    button2.onclick = sellWeapon;
+  }
+}
