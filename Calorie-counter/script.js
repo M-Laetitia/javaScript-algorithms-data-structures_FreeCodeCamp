889952276a05e04b383b616eafea5770e422eb9d
M-Laetitia <molin.laetitia@gmail.com>
2024-05-08 11:04:27 +0200

    Feat: create addEntry function

--- Calorie-counter/script.js
+++ Calorie-counter/script.js
@@ -14,5 +14,20 @@ function cleanInputString(str) {
 function isInvalidInput(str) {
     // const regex = /[0-9]+e[0-9]+/i;
     const regex = /\d+e\d+/i;
-    return str.match(regex);
+    return str.match(regex); // The match method returns an array with any matches found in the string. Should return null when the input is a valid number without any scientific notation.
+}
+
+/* [ '1e3', index: 0, input: '1e3', groups: undefined ]
+"1e3" is the matched value against the /\d+e\d+/i regex.
+index: 0 is the index of the matched value in the string.
+input: '1e3' is the original string that was matched.
+groups: undefined are the matched groups, which are not used in this case. You will learn more about groups in a later project.
+*/
+
+function addEntry() {
+    // const targetId = '#' + entryDropdown.value;
+    // const targetInputContainer = document.querySelector(`${targetId} .input-container`);
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+    entryNumber = targetInputContainer.querySelectorAll(); // The querySelectorAll() method returns a NodeList of all the elements that match the selector. A NodeList is an array-like object, so you can access the elements using bracket notation.
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
 }
