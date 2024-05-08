1daee57183aa08a70874321ba5f1bf54556716da
M-Laetitia <molin.laetitia@gmail.com>
2024-05-08 12:46:53 +0200

    note js

---
+++ note.js
@@ -0,0 +1,9 @@
+Number() // The Number constructor is a function that converts a value to a number. 
+
+insertAdjacentHTML("beforeend", str);// first argument is a string that specifies the position of the inserted element. The second argument is a string containing the HTML to be inserted.
+
+function calculateCalories(e) {
+  e.preventDefault();
+}
+
+Math.abs() // is a built-in JavaScript method that will return the absolute value of a number.
