66de7f8a3e10db37ef658f557368979de412d51d
M-Laetitia <molin.laetitia@gmail.com>
2024-05-09 15:27:36 +0200

    Feat: create deleteSong function

--- Music-player/script.js
+++ Music-player/script.js
@@ -115,6 +115,24 @@ const shuffle = () => {
   setPlayerDisplay();
   setPlayButtonAccessibleText();
 };
+shuffleButton.addEventListener('click', shuffle)
+
+const deleteSong = (id) => {
+  if (userData?.currentSong?.id === id) {
+  userData.currentSong = null;
+  userData.songCurrentTime = 0;
+
+  pauseSong();
+  setPlayerDisplay();
+}
+
+userData.songs = userData?.songs.filter((song) => song.id !== id);
+renderSongs(userData?.songs); 
+highlightCurrentSong(); 
+setPlayButtonAccessibleText(); 
+
+};
+
 
 const setPlayerDisplay = () => {
   const playingSong = document.getElementById("player-song-title");
