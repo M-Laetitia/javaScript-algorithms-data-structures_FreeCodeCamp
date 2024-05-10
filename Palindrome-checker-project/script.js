2df54716b3578de9bffcd7ed6d84b4ef4e749591
M-Laetitia <molin.laetitia@gmail.com>
2024-05-11 01:55:27 +0200

    Feat: add trim for the input.value + return to stop the execution of the fonction

--- Palindrome-checker-project/script.js
+++ Palindrome-checker-project/script.js
@@ -7,9 +7,10 @@ function formattedInput(input) {
 }
 
 checkBtn.addEventListener('click', () => {
-    const textInputValue = textInput.value.trim();
+    const textInputValue = textInput.value.trim(); // trim() to remove leading and trailing whitespace from the text.
   if(textInputValue == '') {
-    alert("Please input a value")
+    alert("Please input a value");
+    return; // to stop the execution of the function 
   }
 
   const cleanedInput = formattedInput(textInputValue);
@@ -20,6 +21,5 @@ checkBtn.addEventListener('click', () => {
   } else {
     result.innerText = `${textInputValue} is not a palindrome`
   }
-
 })
 
