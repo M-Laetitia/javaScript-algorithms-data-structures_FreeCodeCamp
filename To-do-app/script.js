e5d448c513969ea552bea1461c021cc77711f47c
M-Laetitia <molin.laetitia@gmail.com>
2024-05-11 15:07:34 +0200

    Refactor : tranform the input value into a hyphenated string

--- To-do-app/script.js
+++ To-do-app/script.js
@@ -36,6 +36,6 @@ taskForm.addEventListener('submit', (e) => {
     e.preventDefault(); // to stop the browser from refreshing the page after submitting the form.
     const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id) // implicit return
     const taskObj = {
-        id: titleInput.value.toLowerCase()
+        id: titleInput.value.toLowerCase().split(" ").join("-") // to get a final result with a hyphenated string.
     };
 });
