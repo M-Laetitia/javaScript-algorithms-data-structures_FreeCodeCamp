c727ff68737de8b62d868db0d609e5b4666ebd97
M-Laetitia <molin.laetitia@gmail.com>
2024-05-11 12:36:26 +0200

    Feat: add a swtich case to filter the cards on the page

--- Football-team-cards/script.js
+++ Football-team-cards/script.js
@@ -212,4 +212,31 @@ playersDropdownList.addEventListener("change", (e) => {
 
     // reset the content for the playerCards element.
     playerCards.innerHTML = ""
+
+    switch (e.target.value) {
+        case "nickname":
+          setPlayerCards(players.filter((player) => player.nickname !== null));
+          break;
+        case "forward":
+          setPlayerCards(players.filter((player) => player.position === "forward"));
+          break;
+        case "midfielder":
+          setPlayerCards(
+            players.filter((player) => player.position === "midfielder")
+          );
+          break;
+        case "defender":
+          setPlayerCards(
+            players.filter((player) => player.position === "defender")
+          );
+          break;
+        case "goalkeeper":
+          setPlayerCards(
+            players.filter((player) => player.position === "goalkeeper")
+          );
+          break;
+        default :
+          setPlayerCards();
+          break;
+    }
 })
