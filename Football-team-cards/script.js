f864dc0fa995109be1538be67a91b47cb5c13838
M-Laetitia <molin.laetitia@gmail.com>
2024-05-11 11:32:57 +0200

    Feat: add setPlayerCards function that will filter players

--- Football-team-cards/script.js
+++ Football-team-cards/script.js
@@ -184,3 +184,10 @@ typeOfSport.textContent = sport;
 teamName.textContent = team;
 worldCupYear.textContent = year;
 headCoach.textContent = coachName;
+
+
+// show player cards based on the selections made by the user in the Filter Teammates dropdown menu.
+// creating an empty arrow function called setPlayerCards
+const setPlayerCards = (arr = players) => {
+    
+}
