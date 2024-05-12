d41a305294ebd8fdb478fdd4e44acdea8bbabf9f
M-Laetitia <molin.laetitia@gmail.com>
2024-05-12 22:26:28 +0200

    Feat: add other conditions ( if number is negative or is equal to or greater than 4000

--- Roman-numeral-converter-project/script.js
+++ Roman-numeral-converter-project/script.js
@@ -9,4 +9,14 @@ convertBtn.addEventListener('click', () => {
       output.innerText = "Please enter a valid number";
       return;
     };
+  
+    if(numberValue < 0) {
+      output.innerText = "Please enter a number greater than or equal to 1";
+      return;
+    }
+  
+    if(numberValue >= 4000) {
+      output.innerText = "Please enter a number less than or equal to 3999";
+      return;
+    }
 });
