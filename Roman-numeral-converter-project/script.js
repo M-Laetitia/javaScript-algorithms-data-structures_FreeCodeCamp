d47d6a8b590eabd80929006566fb64eaa09d0f50
M-Laetitia <molin.laetitia@gmail.com>
2024-05-12 22:20:33 +0200

    Feat: check if the user submit a number, if not display a msg

--- Roman-numeral-converter-project/script.js
+++ Roman-numeral-converter-project/script.js
@@ -1,3 +1,12 @@
 const convertBtn = document.getElementById('convert-btn');
 const number = document.getElementById('number');
 const output = document.getElementById('output')
+
+
+convertBtn.addEventListener('click', () => {
+    const numberValue = Number(number.value); 
+    if (!numberValue) {
+      output.innerText = "Please enter a valid number";
+      return;
+    };
+});
