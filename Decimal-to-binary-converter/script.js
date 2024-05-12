6e4499115de8bc81f2010aa6e33dce3e8fbf7159
M-Laetitia <molin.laetitia@gmail.com>
2024-05-12 12:08:04 +0200

    Feat: convert button and number input are listening for clicks and Enter key presses

--- Decimal-to-binary-converter/script.js
+++ Decimal-to-binary-converter/script.js
@@ -2,3 +2,19 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+const checkUserInput = () => {
+    console.log(numberInput.value);
+};
+
+// Zvent listener to call the function when users click the Convert button. The event listener should listen for click events and take a reference to the checkUserInput function as a callback. Function references are not called with parentheses.
+convertBtn.addEventListener("click", checkUserInput);
+
+// The event listener should listen for keydown events and take an empty arrow function as a callback.
+numberInput.addEventListener("keydown", (e) => {
+  // Whenever an event listener is triggered by an event, an event object is created automatically.
+  console.log(e);
+
+  if(e.key == "Enter"){
+  
+  }
+})
