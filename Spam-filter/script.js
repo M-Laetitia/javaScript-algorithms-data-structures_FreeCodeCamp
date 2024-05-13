fd878f6291cd8f45da1ee3d1834d49244d5d1fd1
M-Laetitia <molin.laetitia@gmail.com>
2024-05-14 01:05:41 +0200

    Feat: add stock alert regex

--- Spam-filter/script.js
+++ Spam-filter/script.js
@@ -28,7 +28,10 @@ const dollarRegex = /[0-9]+ (?:hundred|thousand|million|billion)? dollars/i;
 //  use the $ anchor to match the end of the string.
 const freeRegex = /(?:^|\s)fr[e3][e3] m[o0]n[e3]y(?:$|\s)/i;
 
-const denyList = [helpRegex, dollarRegex, freeRegex];
+// stock alert
+const stockRegex = /(?:^|\s)[s5][t7][o0][c{[(]k [a@4]l[e3]r[t7](?:$|\s)/i;
+
+const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex];
 
 checkMessageButton.addEventListener("click", () => {
     if (messageInput.value === "") {
