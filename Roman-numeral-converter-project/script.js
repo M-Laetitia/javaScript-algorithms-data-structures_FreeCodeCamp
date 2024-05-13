b34e5833c37c3527ac4a38423abc967d519825b3
M-Laetitia <molin.laetitia@gmail.com>
2024-05-13 22:10:36 +0200

    Refactor: change comparison to add a regex and check the str

--- Roman-numeral-converter-project/script.js
+++ Roman-numeral-converter-project/script.js
@@ -20,11 +20,12 @@ const correspondance =  [
 
 convertBtn.addEventListener('click', () => {
     const numberValue = Number(number.value); 
-    if (!numberValue) {
+
+    if (!number.value || number.value.match(/[e.]/g)) {
       output.innerText = "Please enter a valid number";
       return;
     };
-  
+
     if(numberValue < 0) {
       output.innerText = "Please enter a number greater than or equal to 1";
       return;
