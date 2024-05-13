e8955e4ddb6eb733465d6f80fb26a05b631d4dac
M-Laetitia <molin.laetitia@gmail.com>
2024-05-14 01:07:41 +0200

    Feat: add dear friend regex

--- Spam-filter/script.js
+++ Spam-filter/script.js
@@ -30,8 +30,10 @@ const freeRegex = /(?:^|\s)fr[e3][e3] m[o0]n[e3]y(?:$|\s)/i;
 
 // stock alert
 const stockRegex = /(?:^|\s)[s5][t7][o0][c{[(]k [a@4]l[e3]r[t7](?:$|\s)/i;
+// dear friend
+const dearRegex = /(?:^|\s)d[e3][a@4]r fr[i1|][e3]nd(?:$|\s)/i;
 
-const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex];
+const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex, dearRegex];
 
 checkMessageButton.addEventListener("click", () => {
     if (messageInput.value === "") {
