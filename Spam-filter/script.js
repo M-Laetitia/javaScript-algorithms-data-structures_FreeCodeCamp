cbeec8d1a08c1d61be80716d62ea1e26cdd14eba
M-Laetitia <molin.laetitia@gmail.com>
2024-05-14 00:04:20 +0200

    Refactor: use .test() instead of .match() and use the alternate sequence |for the regex

--- Spam-filter/script.js
+++ Spam-filter/script.js
@@ -6,7 +6,13 @@ const checkMessageButton = document.getElementById('check-message-btn');
 // const  isSpam=(msg)=>{
 //     return  false;
 // }
-const isSpam = (msg) => false;
+
+// implicite return
+// const isSpam = (msg) => msg.match(helpRegex); // return an array
+const isSpam = (msg) => helpRegex.test(msg); // return true or false
+
+// use the alternate sequence | to match either please help or assist me.
+const helpRegex = /please help|assist me/i;
 
 checkMessageButton.addEventListener("click", () => {
     if (messageInput.value === "") {
@@ -14,6 +20,7 @@ checkMessageButton.addEventListener("click", () => {
       return;
     }
   
+    // ternary operator to check the truthiness of calling isSpam() and display appropriate msg on the page
     result.textContent = isSpam(messageInput.value)
       ? "Oh no! This looks like a spam message."
       : "This message does not seem to contain any spam.";
