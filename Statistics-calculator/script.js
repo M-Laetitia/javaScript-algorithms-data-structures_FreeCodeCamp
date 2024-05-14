f4adf0e2d423238293bf8f673f90288f61c84592
M-Laetitia <molin.laetitia@gmail.com>
2024-05-14 21:53:09 +0200

    Feat: create the getMean function

--- Statistics-calculator/script.js
+++ Statistics-calculator/script.js
@@ -14,6 +14,18 @@ const calculate =() => {
     // callback function to your .filter() method that returns true if the element is not NaN.
     // const filtered = numbers.filter(el => !isNaN(el));
 
-    
     const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+    const mean = getMean(numbers) 
+}
+
+// ^ Mean
+const getMean = (array) => {
+    const getMean = (array) => {
+        const sum = array.reduce((acc, el) => acc + el, 0); // set an initial value at 0
+        // calculate the mean by dividing the sum of numbers by the count of numbers in the list.
+        mean = sum / array.length
+        return mean;
+    }
+
+    // const getMean = (array) => array.reduce((acc, el) => acc + el, 0)/ array.length;
 }
