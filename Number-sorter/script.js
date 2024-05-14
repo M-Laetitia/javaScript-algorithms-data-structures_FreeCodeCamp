77301cba068324bb95884b113404ffd7d08dac81
M-Laetitia <molin.laetitia@gmail.com>
2024-05-14 10:49:45 +0200

    Feat: finalise the sortInputArray function

--- Number-sorter/script.js
+++ Number-sorter/script.js
@@ -2,9 +2,19 @@ const sortButton = document.getElementById("sort");
 
 const sortInputArray = (event) => {
     event.preventDefault();
-    // const inputValues = document.getElementsByClassName("values-dropdown"); //  returns an array-like object. 
-    // use the spread operator to convert it into an array.
-    const inputValues = [...document.getElementsByClassName("values-dropdown")];
+    //1. const inputValues = document.getElementsByClassName("values-dropdown"); //  returns an array-like object. 
+    //2. const inputValues = [...document.getElementsByClassName("values-dropdown")];// use the spread operator to convert it into an array.
+    //3.  
+    /*const inputValues = [...document.getElementsByClassName("values-dropdown")];
+        inputValues.map((dropdown) => {
+            return dropdown.value
+    })*/
+
+    //  get the values from your select elements. map function to iterate over the array.
+    // 4 const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => dropdown.value)
+
+    // convert result (string) into numbers with Number()
+    const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => Number(dropdown.value));
 
 }
 
