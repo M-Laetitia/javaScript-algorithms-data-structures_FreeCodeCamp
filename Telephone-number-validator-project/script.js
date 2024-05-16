38cb1eef495cccae33364942e3bc659c59dbb254
M-Laetitia <molin.laetitia@gmail.com>
2024-05-16 23:04:47 +0200

    Feat: add eventListener for checkBtn + clearBtn

--- Telephone-number-validator-project/script.js
+++ Telephone-number-validator-project/script.js
@@ -2,3 +2,14 @@ const userInput = document.getElementById('user-input');
 const checkBtn = document.getElementById('check-btn');
 const clearBtn = document.getElementById('clear-btn');
 const results = document.getElementById('results-div');
+
+checkBtn.addEventListener('click', ()=>{
+    if(!userInput.value) {
+      alert('Please provide a phone number');
+    }
+    isValidNumber();
+  })
+  
+  clearBtn.addEventListener('click',()=>{
+    results.innerHTML= "";
+  })
