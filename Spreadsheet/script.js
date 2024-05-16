af079bb2abd2d459789080e124032b633d5d0813
M-Laetitia <molin.laetitia@gmail.com>
2024-05-16 10:55:39 +0200

    Feat: create update function

--- Spreadsheet/script.js
+++ Spreadsheet/script.js
@@ -39,25 +39,42 @@ const range = (start, end) => Array(end - start + 1).fill(start).map((element, i
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
 
 window.onload = () => {
-    const container = document.getElementById("container");
-    const createLabel = (name) => {
-      const label = document.createElement("div");
-      label.className = "label";
-      label.textContent = name;
-      container.appendChild(label);
-    }
-    const letters = charRange("A", "J");
-    letters.forEach(createLabel)
+  const container = document.getElementById("container");
+  const createLabel = (name) => {
+    const label = document.createElement("div");
+    label.className = "label";
+    label.textContent = name;
+    container.appendChild(label);
+  }
+  const letters = charRange("A", "J");
+  letters.forEach(createLabel)
 
-    // range() returns an array, so chain array methods directly to the function call.
-    range(1, 99).forEach(number => {
-        createLabel(number);
-        letters.forEach(letter => {
-            const input = document.createElement('input');
-            input.type = 'text';
-            input.id =  letter + number;
-            input.ariaLabel = letter + number;
-            container.appendChild(input)
-        })
-      })
+  // range() returns an array, so chain array methods directly to the function call.
+  range(1, 99).forEach(number => {
+    createLabel(number);
+    letters.forEach(letter => {
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.id =  letter + number;
+        input.ariaLabel = letter + number;
+
+        //  tell your input elements to call the update function when the value changes. 
+        input.onchange = update;
+
+        container.appendChild(input)
+
+    })
+  })
+}
+
+const update = event => {
+  // Since the update event is running as a change event listener, the event parameter will be a change event.
+  const element = event.target;
+  // /\s/g is a regular expression that matches all whitespace characters globally (g flag).
+  const value = element.value.replace(/\s/g, '');
+
+  // check if the value does not include the id of the element and also checks if the first character of value is =.
+  if (!value.includes(element.id) && value.charAt(0) === "=") {
+
+  }
 }
