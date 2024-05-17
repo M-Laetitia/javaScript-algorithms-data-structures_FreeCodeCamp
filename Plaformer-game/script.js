9f994acf58dd7c25c4c85ecbe53279d178b26138
M-Laetitia <molin.laetitia@gmail.com>
2024-05-18 00:19:13 +0200

    Feat: add variable related to canva / game features

--- Plaformer-game/script.js
+++ Plaformer-game/script.js
@@ -5,3 +5,18 @@ const checkpointScreen  = document.querySelector(".checkpoint-screen");
 
 // child combinator > to target the paragraph element
 const checkpointMessage = document.querySelector('.checkpoint-screen > p');
+
+// Canvas API - getContext method which will provide the context for where the graphics will be rendered.
+const ctx = canvas.getContext("2d");
+// The innerWidth property is a number that represents the interior width of the browser window.
+canvas.width = innerWidth;
+// The innerHeight property is a number that represents the interior height of the browser window.
+canvas.height = innerHeight;
+// variable to apply gravity.
+const gravity = 0.5;
+// keep track of the status for the checkpoint collision detection.
+let isCheckpointCollisionDetectionActive = true;
+
+
+
+// function  to make sure that the size of the elements in the game are responsive and adapt to different screen sizes.
