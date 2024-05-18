a906e38c9d4f9d7e1ec8ab2cedc4cf4129d9dd79
M-Laetitia <molin.laetitia@gmail.com>
2024-05-18 13:52:53 +0200

    Feat: create the player class, with the constructor and set the velocity/width/height properties

--- Plaformer-game/script.js
+++ Plaformer-game/script.js
@@ -18,8 +18,27 @@ const gravity = 0.5;
 let isCheckpointCollisionDetectionActive = true;
 
 
-// function  to make sure that the size of the elements in the game are responsive and adapt to different screen sizes.
+// function to make sure that the size of the elements in the game are responsive and adapt to different screen sizes.
 const proportionalSize = (size) => {
     return innerHeight < 500 ? Math.ceil((size/500) * innerHeight) : size
 };
 
+// characteristics for the main player of the game.
+class Player {
+  constructor() {
+    this.position = {
+      x: proportionalSize(10),
+      y: proportionalSize(400),
+    };
+
+    this.velocity = { //The velocity property will be used to store the player's speed in the x and y directions.
+      x: 0,
+      y: 0,
+    };
+    // set the width and height properties of your class to be proportional to the height of the screen with proportionalSize().
+    this.width =  proportionalSize(40)
+    this.height =  proportionalSize(40)
+  }
+}
+
+    
