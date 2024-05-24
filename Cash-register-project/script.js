167693de033b497798105982bb84e60f5c36bca5
M-Laetitia <molin.laetitia@gmail.com>
2024-05-24 19:28:03 +0200

    Feat: function to display the drawer cash

--- Cash-register-project/script.js
+++ Cash-register-project/script.js
@@ -41,56 +41,13 @@ purcharseBtn.addEventListener('click', ()=>{
   checkCashRegister();
 });
 
-///
-let price = 1.87;
-let cid = [
-  ["PENNY", 1.01],
-  ["NICKEL", 2.05],
-  ["DIME", 3.1],
-  ["QUARTER", 4.25],
-  ["ONE", 90],
-  ["FIVE", 55],
-  ["TEN", 20],
-  ["TWENTY", 60],
-  ["ONE HUNDRED", 100]
-];
- 
- 
- const purcharseBtn = document.getElementById('purchase-btn'); 
- const changeDue = document.getElementById('change-due');
- const priceDisplay = document.getElementById('price');
- const changeDueDisplay = document.getElementById('price-due');
 
-priceDisplay.innerHTML = `Total : ${price}`
-
-const checkCashRegister =()=>{
-  const cash = document.getElementById('cash').value;
-  if(Number(cash) < price){
-    alert("Customer does not have enough money to       purchase the item");
-    cash.value = '';
-    return;
-  }
-  if(Number(cash) === price) {
-    changeDue.innerText = "No change due - customer paid with exact cash";
-    cash.value = '';
-    return;
-  }
-
-  let changeDue = (Number(cash) - price).toFixed(2);
-  changeDueDisplay.innerHTML = `Change due: ${changeDue}` ;
-  let status = "";
-  let amount = [0.01,0.05,0.1,0.25,1,5,10,20,100];
-  let totalMoney = 0;
-  cid.forEach((amount)=>{
-    console.log(amount[1]);
-    totalMoney += amount[1];
+const displayDrawerCash =()=>{
+  cid.forEach((money)=>{
+    const name = money[0];
+    const capitalized = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+    cashDrawer.innerHTML += `<p>${capitalized}: $${money[1].toFixed(2)}</p>`;
     
   });
-  console.log(totalMoney.toFixed(2));
 }
-
-
-purcharseBtn.addEventListener('click', ()=>{
-  const cash = document.getElementById('cash').value;
-  checkCashRegister();
-});
+displayDrawerCash();
