0bce47ab05a19f5dad530c821b4f97b42ddaadd0
M-Laetitia <molin.laetitia@gmail.com>
2024-05-25 14:41:04 +0200

    Feat: function to populate the UI with the author data+ assign the authorDataArr to data

--- Authors-page/script.js
+++ Authors-page/script.js
@@ -1,7 +1,7 @@
 const authorContainer = document.getElementById('author-container');
 const loadMoreBtn = document.getElementById('load-more-btn');
 
-
+// add 8 authors at a time, and have a button to add 8 more until there's no more data to display
 let startingIndex  = 0;
 let endingIndex = 8;
 let authorDataArr = [];
@@ -14,7 +14,8 @@ fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
     .then((res) => res.json())
     //  to start working with the data,  use another .then() method.
     .then((data) => {
-        console.log(data);
+        authorDataArr = data; //  assign the author data to the empty authorDataArr array
+        console.log("Author Data Array:", authorDataArr)
     })
     // use catch() to handle errors  in case the Promise gets rejected.
     .catch((err) => {
@@ -22,4 +23,16 @@ fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
     }); // terminate the code with a semicolon. You couldn't do that in the previous steps because you'll signal to JavaScript to stop parsing your code, which will affect the fetch() syntax.
 
 
-    // add 8 authors at a time, and have a button to add 8 more until there's no more data to display
+// function to populate the UI with the author data.
+const displayAuthors =(authors)=>{
+    // For the first parameter of the callback, destructure the author, image, url, and bio items. For the second parameter, pass in index. This will represent the position of each author, and will be useful for pagination later.
+    authors.forEach(({ author, image, url, bio }, index)=>{
+        //  start building the HTML for the page with your destructured data. 
+        authorContainer.innerHTML += `
+        <div id="${index}" class="user-card">
+            <h2 class="author-name">${author}</h2>
+        </div>
+        `;
+    });
+}
+
