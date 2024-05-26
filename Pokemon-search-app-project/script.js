cd18a251ccccae88cd66bd931f6d86711577e218
M-Laetitia <molin.laetitia@gmail.com>
2024-05-26 15:22:46 +0200

    Feat: fetch Pokémon data with an asynchronous function

--- Pokemon-search-app-project/script.js
+++ Pokemon-search-app-project/script.js
@@ -13,7 +13,29 @@ const speed = document.getElementById('speed');
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
 
+// Define an asynchronous function to fetch Pokémon data
+const getPokemon = async () => {
+  try {
+    // Get the Pokémon name or ID from the input field and convert it to lowercase
+    const pokemonNameOrId = searchInput.value.toLowerCase();
 
+    // Fetch data from the Pokémon API using the name or ID
+    // The await keyword pauses the function execution until the fetch promise is resolved
+    const response = await fetch(
+      `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${pokemonNameOrId}`
+    );
+
+    // Await the response and parse it as JSON
+    // This step also pauses the function execution until the promise is resolved
+    const data = await response.json();
+
+  } catch (err) {
+    // If an error occurs during the fetch or JSON parsing, the catch block handles it and display an alert message.
+    alert('Pokémon not found');
+    // Log the error message to the console for debugging purposes
+    console.log(`Pokémon not found: ${err}`);
+  }
+};
 
 searchForm.addEventListener('submit', e => {
   e.preventDefault();
