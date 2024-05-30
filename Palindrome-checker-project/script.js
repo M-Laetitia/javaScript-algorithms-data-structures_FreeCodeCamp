8a90482d155438b92db6e3fea63de63a871efb1d
M-Laetitia <molin.laetitia@gmail.com>
2024-05-30 23:17:33 +0200

    Feat: add different msg if it's a palindrome or not (icon + color)

--- Palindrome-checker-project/script.js
+++ Palindrome-checker-project/script.js
@@ -1,12 +1,15 @@
 const checkBtn = document.getElementById('check-btn')
 const textInput = document.getElementById('text-input')
 const result = document.getElementById('result')
+const title = document.querySelector('h1')
 
 function formattedInput(input) {
     return input.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "").toLowerCase().split("").join('');
 }
 
 checkBtn.addEventListener('click', () => {
+    title.style.color = '#1f2159';
+
     const textInputValue = textInput.value.trim(); // trim() to remove leading and trailing whitespace from the text.
   if(textInputValue == '') {
     alert("Please input a value");
@@ -17,9 +20,16 @@ checkBtn.addEventListener('click', () => {
   const reversedInput = cleanedInput.split('').reverse().join('');
 
   if(cleanedInput === reversedInput) {
-    result.innerText = `${textInputValue} is a palindrome`
+    result.innerHTML = `<i class="ri-check-line"></i> ${textInputValue} is a palindrome`
+    result.style.color = '#0f54a2';
   } else {
-    result.innerText = `${textInputValue} is not a palindrome`
+    result.innerHTML = `<i class="ri-close-line"></i> ${textInputValue} is not a palindrome`
+    result.style.color = '#fe618b';
   }
 })
 
+textInput.addEventListener('click', () =>{
+  title.style.color = '#cc236a';
+})
+
+// <i class="ri-close-line"></i>
